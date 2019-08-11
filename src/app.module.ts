ded2315fd71b6b1fa917ca5cdd21d90860de67b6
Erkan Ertürk <erkanerturk096@gmail.com>
2019-08-11 13:23:57 +0300

    HTTP Filter && Validation Pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+
+import { typeOrmConfig } from './config/typeorm.config';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule { }
